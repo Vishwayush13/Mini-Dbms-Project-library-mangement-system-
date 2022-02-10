37ca071ac58523621760115aba419c22bd0d22cf
Vishwayush13 <88092271+Vishwayush13@users.noreply.github.com>
2022-02-10 11:51:37 +0530

    Update transactionuserconrol.cs

--- transactionuserconrol.cs
+++ transactionuserconrol.cs
@@ -55,8 +55,8 @@ namespace Library_Management_System
 
        
 
-        private void searchbuttonborrow_Click(object sender, EventArgs e)
-        {   // to get book1 value 
+        private void searchbuttonborrow_Click(object sender, EventArgs e){
+          // to get book1 value 
             con.Open();
             string syntax="SELECT book1 FROM Borrow_by where br_id ='"+borrowertextbox.Text+"'";
             cmd = new SqlCommand(syntax, con);  
@@ -65,7 +65,6 @@ namespace Library_Management_System
             Bklabel1.Text=book1=DR[0].ToString();
             con.Close();
             // to get book2 value 
-
             con.Open();
              syntax = "SELECT book2 FROM Borrow_by where br_id ='"+borrowertextbox.Text+"'";
             cmd = new SqlCommand(syntax, con);
@@ -73,10 +72,7 @@ namespace Library_Management_System
             DR.Read();
             BKlabel2.Text = book2 = DR[0].ToString();
             con.Close();
-
-
-
-        }
+          }
         private void search_buttonbook_Click(object sender, EventArgs e)
         {
             // to get borrower 
@@ -101,81 +97,59 @@ namespace Library_Management_System
 
         }
 
-        private void Issuebook_Click(object sender, EventArgs e)
-        {
-
+        private void Issuebook_Click(object sender, EventArgs e){
             // to see if book is borrowed by someone 
             searchbuttonborrow.PerformClick();
             //search_buttonbook.PerformClick();
-            if (borrower != "")
-            {
+            if (borrower != ""){
                 // some has borrowed the book
                 MessageBox.Show("Book is already borrowed by some other borrower with borrower's id: \n " + borrower);
-                return;
-            }
+                return;}
             // see if borrower has already taken 2 books 
             searchbuttonborrow.PerformClick();
-
-            if((book1 !="") && (book2 != ""))
-            {
+            if((book1 !="") && (book2 != "")){
                 // borrower has already borrowed maximum no of books
                 MessageBox.Show("Borrower has already borrowed max no of books");
-                return;
-            }
+                return;}
             // now proceed with issue logic
             // set value of book1 and book2 as bookid of book being issued
-            try
-            {
-                if(book1 == "")
-                {
+            try{
+                 if(book1 == ""){
                     // book1 must be updated in book1 slot
-                    cmd=new SqlCommand("Trans_update_book1_SP", con);
-
-                }
-                else
-                {
-                    cmd = new SqlCommand("Trans_update_book2_SP", con);
-                }
+                    cmd=new SqlCommand("Trans_update_book1_SP", con);}
+                else{
+                    cmd = new SqlCommand("Trans_update_book2_SP", con);}
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@book_id",bookIdtextbox.Text);
                 cmd.Parameters.AddWithValue("@br_id",borrowertextbox.Text);
                 con.Open();
-                try
-                {
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
+                try{
+                    cmd.ExecuteNonQuery();}
+                catch (Exception ex){
                     MessageBox.Show(">>> INVALID QUERY \n" + ex);
-                }
+                }}
                 con.Close();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(" " + ex);
+            catch(Exception ex){
+            MessageBox.Show(" " + ex);
             }
             // now we will also update the brower's id in book table
             cmd = new SqlCommand("Trans_update_borrower_SP", con);
             cmd.CommandType=CommandType.StoredProcedure;
-
             cmd.Parameters.AddWithValue("@book_id", bookIdtextbox.Text);
             cmd.Parameters.AddWithValue("@br_id", borrowertextbox.Text);
             con.Open();
-            try
-            {
-                cmd.ExecuteNonQuery();
-            }
-            catch( Exception ex)
-            {
-                MessageBox.Show(">>>> INVALID QUERY \n" + ex);
-            }
+            try{
+            cmd.ExecuteNonQuery();}
+            catch( Exception ex){
+              MessageBox.Show(">>>> INVALID QUERY \n" + ex);}
             con.Close();
             search_buttonbook.PerformClick();
             searchbuttonborrow.PerformClick();
             MessageBox.Show("ISSUED SUCCESSFULLY !!!");
+            }
 
 
-        }
+       
 
         private void returnButton_Click(object sender, EventArgs e)
         {
